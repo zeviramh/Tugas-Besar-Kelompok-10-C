aa92e10500fd88a21b2ff7807225d2a68cd0cd53
nazla <nazlafaradisa28@gmail.com>
2024-12-18 16:59:12 +0700

    revisi font setelah presentasi

--- sistem_parkir_final.py
+++ sistem_parkir_final.py
@@ -15,7 +15,6 @@ data_parkir = {"jenis_kendaraan": None, "nomor_plat": None, "waktu_masuk": None,
 # Variabel global untuk menyimpan data kendaraan yang tercatat
 data_parkir_tercatat = {}
 
-
 # Fungsi untuk memeriksa username dan password
 def check_login(username, password):
     try:
@@ -46,15 +45,19 @@ def kendaraan_masuk():
     
     set_background(root)
     
-    tk.Label(root, text="Kendaraan Masuk", bg= "#FFACC5", fg= "black").pack()
+    kendaraan_masuk_label = tk.Label(root, text="Kendaraan Masuk", font=("Cascadia Code", 20, "bold"), bg= "#FFACC5", fg= "black")
+    kendaraan_masuk_label.place(relx=0.5, rely=0.1, anchor='center')
     kendaraan_var = tk.StringVar(value="motor")
     
-    tk.Radiobutton(root, text="Motor", variable=kendaraan_var, value="motor", bg= "#FFACC5", fg= "black").pack()
-    tk.Radiobutton(root, text="Mobil", variable=kendaraan_var, value="mobil", bg= "#FFACC5", fg= "black").pack()
+    radio_motor = tk.Radiobutton(root, text="Motor",font=("Cascadia Code", 16), variable=kendaraan_var, value="motor", bg= "#FFACC5", fg= "black")
+    radio_motor.place(relx=0.5, rely=0.2, anchor='center')
+    radio_mobil = tk.Radiobutton(root, text="Mobil",font=("Cascadia Code", 16), variable=kendaraan_var, value="mobil", bg= "#FFACC5", fg= "black")
+    radio_mobil.place(relx=0.5, rely=0.3, anchor='center')
 
-    tk.Label(root, text="Masukkan Nomor Plat", bg= "#FFACC5", fg= "black").pack()
-    nomor_plat_entry = tk.Entry(root)
-    nomor_plat_entry.pack()
+    nomor_plat_label = tk.Label(root, text="Masukkan Nomor Plat", font=("Cascadia Code", 18, ), bg= "#FFACC5", fg= "black")
+    nomor_plat_label.place(relx=0.5, rely=0.4, anchor='center')
+    nomor_plat_entry = tk.Entry(root, font=("Cascadia Code", 16))
+    nomor_plat_entry.place(relx=0.5, rely=0.5, anchor='center')
     
     # Generate nomor karcis dan simpan di data_parkir
     nomor_karcis = generate_nomor_karcis()
@@ -64,7 +67,6 @@ def kendaraan_masuk():
     def simpan_masuk():
         data_parkir["jenis_kendaraan"] = kendaraan_var.get()
         data_parkir["nomor_plat"] = nomor_plat_entry.get().strip()
-        
         
         # Validasi nomor plat kendaraan
         if not data_parkir["nomor_plat"]:
@@ -89,8 +91,10 @@ def kendaraan_masuk():
         }
     
         waktu_masuk_pintu_masuk()
-    tk.Button(root, text="Simpan", command=simpan_masuk).pack()
-    tk.Button(root, text="Kembali", command=kembali_ke_menu).pack()
+    simpan_button = tk.Button(root, text="Simpan", font=("Cascadia Code", 14), command=simpan_masuk)
+    simpan_button.place(relx=0.5, rely=0.6, anchor='center')
+    kembali_button = tk.Button(root, text="Kembali",font=("Cascadia Code", 14), command=kembali_ke_menu)
+    kembali_button.place(relx=0.5, rely=0.7, anchor='center')
     
 def waktu_masuk_pintu_masuk():
     for widget in root.winfo_children():
@@ -98,9 +102,10 @@ def waktu_masuk_pintu_masuk():
             
     set_background(root)
         
-    tk.Label(root, text="Waktu Masuk", bg= "#FFACC5", fg= "black").pack(pady=10)
-    waktu_label = tk.Label(root, text="",bg= "pink", fg= "black")
-    waktu_label.pack()
+    waktu_masuk_label = tk.Label(root, text="Waktu Masuk", font=("Cascadia Code", 20, "bold"), bg= "#FFACC5", fg= "black")
+    waktu_masuk_label.place(relx=0.5, rely=0.2, anchor='center')
+    waktu_label = tk.Label(root, text="",font=("Cascadia Code", 16),bg= "pink", fg= "black")
+    waktu_label.place(relx=0.5, rely=0.3, anchor='center')
     
     def capture_waktu_masuk():
         now = datetime.now()
@@ -117,10 +122,11 @@ def waktu_masuk_pintu_masuk():
         data_parkir["hari_masuk"] = hari_masuk
      
         
-    capture_button = tk.Button(root, text="Tekan Tombol Masuk", command=capture_waktu_masuk)
-    capture_button.pack(pady=10)
+    capture_button = tk.Button(root, text="Tekan Tombol Masuk",font=("Cascadia Code", 16), command=capture_waktu_masuk)
+    capture_button.place(relx=0.5, rely=0.4, anchor='center')
     
-    cetak_button = tk.Button(text="Cetak Karcis", state="disabled", command=lambda: cetak_karcis())
+    cetak_button = tk.Button(text="Cetak Karcis",font=("Cascadia Code", 16), state="disabled", command=lambda: cetak_karcis())
+    cetak_button.place(relx=0.5, rely=0.5, anchor='center')
       
     def cetak_karcis():
         jenis_kendaraan = data_parkir.get("jenis_kendaraan", "Tidak diketahui")
@@ -141,7 +147,6 @@ def waktu_masuk_pintu_masuk():
             f"Nomor Karcis: {nomor_karcis}")
         
         kembali_ke_menu()
-    cetak_button.pack(pady=10)
     
 def simpan_data(waktu_window):
     # Menyimpan data dan menutup jendela waktu
@@ -151,7 +156,7 @@ def simpan_data(waktu_window):
     
 # Fungsi untuk menyimpan data ke CSV
 csv_file = "database.csv"
-def simpan_ke_csv(jenis_kendaraan, nomor_plat, tanggal_masuk, waktu_masuk, waktu_keluar,lama_parkir, harga_parkir):
+def simpan_ke_csv(nomor_karcis,jenis_kendaraan, nomor_plat, tanggal_masuk, waktu_masuk, waktu_keluar,lama_parkir, harga_parkir):
     # Jika file belum ada, buat file baru dengan header
     try:
         header = ["Nomor Karcis", "Jenis Kendaraan", "Nomor Plat", "Tanggal Masuk", "Waktu Masuk", "Waktu Keluar", "Lama Parkir", "Harga Parkir"]
@@ -169,10 +174,10 @@ def simpan_ke_csv(jenis_kendaraan, nomor_plat, tanggal_masuk, waktu_masuk, waktu
             writer = csv.writer(f)
             if file_baru:
                 writer.writerow(header)  # Tulis header jika file baru
-            writer.writerow([jenis_kendaraan, nomor_plat, tanggal_masuk, waktu_masuk, waktu_keluar, lama_parkir, harga_parkir])
-        messagebox.showinfo("Sukses", f"Data berhasil disimpan ke {csv_file}")
+            writer.writerow([nomor_karcis,jenis_kendaraan, nomor_plat, tanggal_masuk, waktu_masuk, waktu_keluar, lama_parkir, harga_parkir])
+        messagebox.showinfo("Sukses", f"Data berhasil disimpan")
     except Exception as e:
-        messagebox.showerror("Error", f"Gagal menyimpan data ke CSV: {str(e)}")
+        messagebox.showerror("Error", f"Gagal menyimpan data")
     
  # Fungsi untuk kendaraan keluar
 def kendaraan_keluar():
@@ -181,11 +186,12 @@ def kendaraan_keluar():
     
     set_background(root)
         
-    tk.Label(root, text="Kendaraan Keluar", bg= "#FFACC5", fg= "black").pack()
-    
-    tk.Label(root, text="Masukkan Nomor Karcis", bg="#FFACC5", fg="black").pack()
-    nomor_karcis_entry = tk.Entry(root)
-    nomor_karcis_entry.pack()
+    kendaraan_keluar_label = tk.Label(root, text="Kendaraan Keluar", font=("Cascadia Code", 20 , "bold"), bg= "#FFACC5", fg= "black")
+    kendaraan_keluar_label.place(relx=0.5, rely=0.2, anchor='center')
+    nomor_karcis_label = tk.Label(root, text="Masukkan Nomor Karcis", font=("Cascadia Code", 16), bg="#FFACC5", fg="black")
+    nomor_karcis_label.place(relx=0.5, rely=0.3, anchor='center')
+    nomor_karcis_entry = tk.Entry(root, font=("Cascadia Code", 16))
+    nomor_karcis_entry.place(relx=0.5, rely=0.4, anchor='center')
 
     def cari_data_karcis():
         nomor_karcis = nomor_karcis_entry.get().strip()
@@ -207,20 +213,20 @@ def kendaraan_keluar():
         tanggal_masuk = data.get("tanggal_masuk", "Tidak diketahui")
         waktu_masuk = data.get("waktu_masuk", "Tidak diketahui")
 
-        messagebox.showinfo(
-            "Data Karcis",
+        messagebox.showinfo("Data Karcis",
             f"Nomor Karcis: {nomor_karcis}\n"
             f"Jenis Kendaraan: {jenis_kendaraan}\n"
             f"Nomor Plat: {nomor_plat}\n"
             f"Tanggal Masuk: {tanggal_masuk}\n"
-            f"Waktu Masuk: {waktu_masuk}"
-        )
+            f"Waktu Masuk: {waktu_masuk}")
         
         # Lanjut ke proses waktu keluar
         waktu_keluar()
 
-    tk.Button(root, text="Cari Data Karcis", command=cari_data_karcis).pack()
-    tk.Button(root, text="Kembali", command=kembali_ke_menu).pack()
+    cari_data_karcis_button = tk.Button(root, text="Cari Data Karcis", font=("Cascadia Code", 16), command=cari_data_karcis)
+    cari_data_karcis_button.place(relx=0.5, rely=0.5, anchor='center')
+    kembali_button = tk.Button(root, text="Kembali",font=("Cascadia Code", 16), command=kembali_ke_menu)
+    kembali_button.place(relx=0.5, rely=0.6, anchor='center')
     
 def waktu_keluar():
     for widget in root.winfo_children():
@@ -228,11 +234,12 @@ def waktu_keluar():
             
     set_background(root)
     
-    tk.Label(root, text="Waktu Keluar", fg= "black", bg= "pink").pack(pady=10)
+    waktu_keluar_label = tk.Label(root, text="Waktu Keluar", font=("Cascadia Code", 18), fg= "black", bg= "pink")
+    waktu_keluar_label.place(relx=0.5, rely=0.1, anchor='center')
 
-    waktu_keluar_entry = tk.Entry(root)
-    tanggal_keluar_entry = tk.Entry(root)
-    hari_label = tk.Label(root, text="")
+    waktu_keluar_entry = tk.Entry(root, font= ("Cascadia Code", 16))
+    tanggal_keluar_entry = tk.Entry(root, font= ("Cascadia Code", 16))
+    hari_label = tk.Label(root, text="",font= ("Cascadia Code", 16))
 
     def capture_waktu_keluar():
         now = datetime.now()
@@ -242,18 +249,20 @@ def waktu_keluar():
         tanggal_keluar_entry.insert(0, now.strftime("%d-%m-%Y"))
         hari_label.config(text=f"Hari: {now.strftime('%A')}")
 
-    capture_button = tk.Button(root, text="Serahkan Karcis", command=capture_waktu_keluar)
-    capture_button.pack(pady=10)
+    capture_button = tk.Button(root, text="Serahkan Karcis", font= ("Cascadia Code", 16), command=capture_waktu_keluar)
+    capture_button.place(relx=0.5, rely=0.2, anchor='center')
 
-    tk.Label(root, text="Waktu Keluar (HH.MM)",bg= "pink", fg= "black").pack()
-    waktu_keluar_entry= tk.Entry(root, bg= "lightgrey", fg= "black")
-    waktu_keluar_entry.pack()
+    waktu_keluar_label = tk.Label(root, text="Waktu Keluar (HH.MM)",font= ("Cascadia Code", 16),bg= "pink", fg= "black")
+    waktu_keluar_label.place(relx=0.5, rely=0.3, anchor='center')
+    waktu_keluar_entry= tk.Entry(root, font= ("Cascadia Code", 16), bg= "lightgrey", fg= "black")
+    waktu_keluar_entry.place(relx=0.5, rely=0.4, anchor='center')
 
-    tk.Label(root, text="Tanggal Keluar (DD-MM-YYYY)",bg= "pink", fg= "black").pack()
-    tanggal_keluar_entry= tk.Entry(root, bg= "lightgrey", fg= "black")
-    tanggal_keluar_entry.pack()
+    tanggal_keluar_label = tk.Label(root, text="Tanggal Keluar (DD-MM-YYYY)", font= ("Cascadia Code", 16),bg= "pink", fg= "black")
+    tanggal_keluar_label.place(relx=0.5, rely=0.5, anchor='center')
+    tanggal_keluar_entry= tk.Entry(root, font= ("Cascadia Code", 16), bg= "lightgrey", fg= "black")
+    tanggal_keluar_entry.place(relx=0.5, rely=0.6, anchor='center')
 
-    hari_label.pack()   
+    hari_label.place(relx=0.5, rely=0.7, anchor='center')  
  
     def simpan_waktu_keluar():
         tanggal_masuk = data_parkir.get("tanggal_masuk", "")
@@ -273,7 +282,8 @@ def waktu_keluar():
         except ValueError:
             messagebox.showerror("Error", "Format waktu atau tanggal tidak valid.")
 
-    tk.Button(root, text="Hitunglah Biaya Parkir", command=simpan_waktu_keluar).pack(pady=10)  
+    biaya_parkir_label = tk.Button(root, text="Hitunglah Biaya Parkir",font= ("Cascadia Code", 16), command=simpan_waktu_keluar)
+    biaya_parkir_label.place(relx=0.5, rely=0.8, anchor='center')
     
  # Fungsi untuk menghitung biaya parkir
 def hitung_biaya_parkir():
@@ -306,7 +316,7 @@ def hitung_biaya_parkir():
         else:
             messagebox.showerror("Error", "Jenis kendaraan tidak valid.")
             return
-        simpan_ke_csv (jenis_kendaraan, nomor_plat, data_parkir["tanggal_masuk"], data_parkir["waktu_masuk"], data_parkir["waktu_keluar"], lama_parkir, harga_parkir)
+        simpan_ke_csv (data_parkir["nomor_karcis"],jenis_kendaraan, nomor_plat, data_parkir["tanggal_masuk"], data_parkir["waktu_masuk"], data_parkir["waktu_keluar"], lama_parkir, harga_parkir)
         
         # Tampilkan hasil
         messagebox.showinfo("Hasil",
@@ -329,9 +339,12 @@ def kembali_ke_menu():
         
     set_background(root)
 
-    tk.Label(root, text="Sistem Parkir", font=("Arial", 16), bg= "#FFACC5", fg= "black").pack(pady=10, padx=50)
-    tk.Button(root, text="Kendaraan Masuk", command=kendaraan_masuk, bg= "#FFACC5", fg= "black").pack(pady=10, padx=50)
-    tk.Button(root, text="Kendaraan Keluar", command=kendaraan_keluar, bg= "#FFACC5", fg= "black").pack(pady=10, padx=50)   
+    pilihan_menu_label = tk.Label(root, text="PILIHAN MENU", font= ("Cascadia Code", 28, "bold"), bg= "#FFACC5", fg= "black")
+    pilihan_menu_label.place(relx=0.5, rely=0.3, anchor='center')
+    kendaraan_masuk_button = tk.Button(root, text="Kendaraan Masuk",font= ("Cascadia Code", 16), command=kendaraan_masuk, bg= "lightgrey", fg= "black")
+    kendaraan_masuk_button.place(relx=0.5, rely=0.4, anchor='center')
+    kendaraan_keluar_button = tk.Button(root, text="Kendaraan Keluar", font= ("Cascadia Code", 16),command=kendaraan_keluar, bg= "lightgrey", fg= "black")
+    kendaraan_keluar_button.place(relx=0.5, rely=0.5, anchor='center')
                 
 def set_background(window):
     image_path = "pinkbg.png"  # Ganti dengan nama file gambar Anda
@@ -345,20 +358,25 @@ def set_background(window):
 # Main window
 root = tk.Tk()
 root.title("Sistem Parkir")
-root.geometry("700x500")
+root.geometry("1920x1080")
+
+width = root.winfo_width()
+height = root.winfo_height()
 
 set_background(root)
 
 # Membuat form login
-tk.Label(root, text="Username:", bg= "#FFACC5", fg= "black").pack(pady=10, padx=50)
-username_entry = tk.Entry(root)
-username_entry.pack(pady=10, padx=50)
+username_label = tk.Label(root, text="Username:",font=("Cascadia Code", 16), bg= "#FFACC5", fg= "black") 
+username_label.place(relx=0.5, rely=0.2, anchor='center')
+username_entry = tk.Entry(root, font=("Cascadia Code", 16))
+username_entry.place(relx=0.5, rely=0.3, anchor='center')
 
-tk.Label(root, text="Password:", bg= "#FFACC5", fg= "black").pack(pady=10, padx=50)
-password_entry = tk.Entry(root, show="*")
-password_entry.pack(pady=10, padx=50)
+password_label = tk.Label(root, text="Password:",font=("Cascadia Code", 16), bg= "#FFACC5", fg= "black")
+password_label.place(relx=0.5, rely=0.4, anchor='center')
+password_entry = tk.Entry(root, font=("Cascadia Code", 16), show="*")
+password_entry.place(relx=0.5, rely=0.5, anchor='center')
 
-login_button = tk.Button(root, text="Login", bg= "lightgrey", fg= "black", command=login)
-login_button.pack(pady=10, padx=50)
+login_button = tk.Button(root, text="Login",font=("Cascadia Code", 16), bg= "lightgrey", fg= "black", command=login)
+login_button.place(relx=0.5, rely=0.6, anchor='center')
 
 root.mainloop()   
